d47f863dfc658adec0846484f41650044fb37e3e
GER\sgenish <shimi.g91@gmail.com>
2017-10-13 16:36:43 +0300

    Ordering login page to html,css,js files.

---
+++ ServerSideFinalProject/ServerSideFinalProject/Project_Files/JavaScript/login.js
@@ -0,0 +1,27 @@
+﻿
+onSuccess = function (data) {
+
+    if (data == true)
+        alert("Successfull login\n");
+
+    else
+        alert("Bad username or password, please try again!");
+}
+
+onError = function () {
+
+    alert("error!");
+}
+
+
+
+
+function validateUser() {
+    var user = {
+        user_name: $("#user_id").val(),
+        password: $("#user_pass").val()
+    };
+
+    //making ajax call to UserController 
+    ajaxHandler(Type.get, urlGenerator(["user",user.user_name, user.password]), "", onSuccess, onerror);
+}
