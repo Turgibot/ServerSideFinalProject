542293c5f8f912841d962acfbd2e91cd4e3bdd3d
GER\sgenish <shimi.g91@gmail.com>
2017-10-25 13:54:17 +0300

    adding some Geters to UserInLesson class and controller

--- BackEndGym/ClassLibrary/ClassLibrary/GymClasses/UserInLesson.cs
+++ BackEndGym/ClassLibrary/ClassLibrary/GymClasses/UserInLesson.cs
@@ -8,5 +8,48 @@ namespace ClassLibrary
 {
     public partial class UserInLesson
     {
+
+        public List<UserInLesson> GetUserInLessonByUserId(int user_id)
+        {
+            try
+            {
+                myGymDBConnection db = new myGymDBConnection();
+                return db.UserInLessons.Where(x => x.user_id == user_id).ToList();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public List<UserInLesson> GetUserInLessonByLessonId(string lesson_id)
+        {
+            try
+            {
+                myGymDBConnection db = new myGymDBConnection();
+                return db.UserInLessons.Where(x => x.lesson_id.ToLower() == lesson_id.ToLower()).ToList();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public List<UserInLesson> GetUserInLessonByLessonDay(string lesson_day)
+        {
+            try
+            {
+                myGymDBConnection db = new myGymDBConnection();
+                return db.UserInLessons.Where(x => x.lesson_day.ToLower() == lesson_day.ToLower()).ToList();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        //      lesson_day, lesson_id, lesson,time
+
     }
 }
+
